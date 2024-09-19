60bcd880366da0a6a9b7486f7c1cb59e90eae49a
yamada031016 <ymwk.sota@gmail.com>
2024-09-19 14:01:44 +0900

    add rich ServerHello struct

--- src/tls/handshake/ServerHello.zig
+++ src/tls/handshake/ServerHello.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const tls = std.crypto.tls;
 const ExtensionType = std.crypto.tls.ExtensionType;
 const handshake = @import("handshake.zig");
 const TLSHandshakeError = handshake.TLSHandshakeError;
@@ -9,10 +10,12 @@ const CipherSuite = helloFields.CipherSuite;
 const legacyCompressionMethods = helloFields.legacyCompressionMethods;
 const Extensions = helloFields.Extensions;
 const ExtensionField = helloFields.ExtensionField;
-const ch = @import("ClientHello.zig");
-const RawClientHello = ch.RawClientHello;
+const clientHello = @import("ClientHello.zig");
+const RawClientHello = clientHello.RawClientHello;
+const ClientHello = clientHello.ClientHello;
+const ExtensionList = helloFields.ExtensionList;
 
-pub const ServerHello = extern struct {
+pub const RawServerHello = extern struct {
     version: Version,
     random: [32]u8,
     legacy_session_id_echo: LegacySessionId,
@@ -20,7 +23,7 @@ pub const ServerHello = extern struct {
     legacy_compression_methods: legacyCompressionMethods = legacyCompressionMethods{},
     extensions: Extensions,
 
-    pub fn parse(client_hello: RawClientHello) !ServerHello {
+    pub fn parse(client_hello: RawClientHello) !RawServerHello {
         var random: [32]u8 = undefined;
         std.crypto.random.bytes(&random);
         const cipher_suite = choice: {
@@ -52,13 +55,59 @@ pub const ServerHello = extern struct {
             ExtensionField{ .extension_type = .key_share, .length = [_]u8{ 0, 0, 0x38 }, .extension = &key_share },
         };
         const extensions = Extensions.init(extension);
-        return ServerHello{
+        return RawServerHello{
             .version = Version{ .major = 0x03, .minor = 0x03 },
             .random = random,
             .cipher_suite = CipherSuite{ .count = 1, .cipher_suites = [_]u16{@intFromEnum(cipher_suite)} ** 64 },
             .legacy_session_id_echo = client_hello.legacy_session_id,
             .extensions = extensions,
         };
+    }
+};
+
+pub const ServerHello = struct {
+    version: std.crypto.tls.ProtocolVersion,
+    random: [32]u8,
+    legacy_session_id_echo: LegacySessionId,
+    cipher_suite: std.crypto.tls.CipherSuite,
+    legacy_compression_methods: legacyCompressionMethods = legacyCompressionMethods{},
+    extensions: []ExtensionList,
+
+    const support_suites = [_]std.crypto.tls.CipherSuite{ .AES_256_GCM_SHA384, .AES_128_GCM_SHA256 };
+
+    pub fn init(ch: ClientHello) !ServerHello {
+        var random: [32]u8 = undefined;
+        std.crypto.random.bytes(&random);
+        const cipher_suite = choice: {
+            for (ch.cipher_suite) |suite| {
+                if (std.mem.containsAtLeast(tls.CipherSuite, &support_suites, 1, &[_]tls.CipherSuite{suite})) {
+                    break :choice suite;
+                }
+            }
+            unreachable;
+        };
+        const secret_key = [32]u8{ 0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9a, 0x9b, 0x9c, 0x9d, 0x9e, 0x9f, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf };
+        const key_pair = try std.crypto.dh.X25519.KeyPair.create(secret_key);
+        var ex_list: [32]ExtensionList = undefined;
+        var ex_pos: usize = 0;
+        var version = [_]tls.ProtocolVersion{.tls_1_3};
+        ex_list[ex_pos] = ExtensionList{ .supported_versions = &version };
+        ex_pos += 1;
+        ex_list[ex_pos] = ExtensionList{ .key_share = .{ .group = tls.NamedGroup.x25519, .key_exchange = key_pair.public_key } };
+        ex_pos += 1;
+        return ServerHello{
+            .version = std.crypto.tls.ProtocolVersion.tls_1_2,
+            .random = random,
+            .cipher_suite = cipher_suite,
+            .legacy_session_id_echo = ch.legacy_session_id,
+            .extensions = try std.heap.page_allocator.dupe(ExtensionList, ex_list[0..ex_pos]),
+        };
+    }
+
+    pub fn toBytes(self: *ServerHello) ![]u8 {
+        var header: [5]u8 = undefined;
+        header[0] = @intFromEnum(tls.ContentType.handshake);
+        const version = @intFromEnum(self.version);
     }
 };
 

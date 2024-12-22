bbb9265ac1425488debefd25e4c34d929d0f688b
yamada031016 <ymwk.sota@gmail.com>
2024-12-22 17:38:10 +0900

    make ExecuteOptions public

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,4 @@
 pub const HTTPServer = @import("server.zig").HTTPServer;
 pub const WebSocketManager = @import("websocket.zig").WebSocketManager;
 pub const WebSocketServer = @import("websocket.zig").WebSocketServer;
+pub const ExecuteOptions = @import("main.zig").ExecuteOptions;
